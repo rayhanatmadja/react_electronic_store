672214145c9a046527bade6b8ed19a1b16a12bfb
Ryhn162004 <87838000+Ryhn162004@users.noreply.github.com>
2022-05-15 08:39:20 +0700

    adding other schema

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,13 +1,32 @@
 const express = require("express");
 const router = express.Router();
+const { verifyToken, verifyTokenAndAuth } = require("./verifyToken");
+const CryptoJs = require("crypto-js");
+const UserModel = require("../schema/UserSchema");
 
-router.get("/getuser", (req, res) => {
-  res.send("Get user succesfull");
-});
+router.put(":/id", verifyTokenAndAuth, async (req, res) => {
+  const password = req.body.password;
+  if (password) {
+    // encrypt password
+    password = CryptoJs.AES.encrypt(
+      password,
+      process.env.PASS_SECRET
+    ).toString();
+  }
 
-router.post("/postuser", (req, res) => {
-  const username = req.body.username;
-  res.send(`User added : ${username}`);
+  // update user(filter first by id)
+  try {
+    const updatedUser = await UserModel.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
